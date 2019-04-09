8fea857d870207af10fc219a21bdde3fbdca208f
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2019-04-09 22:06:39 +0200

    Initial commit

---
+++ src/main/java/fr/ukaralchuk/reactiverestspringmongodb/ReactiveRestSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package fr.ukaralchuk.reactiverestspringmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestSpringMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveRestSpringMongodbApplication.class, args);
+    }
+
+}
